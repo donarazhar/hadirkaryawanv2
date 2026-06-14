63aa2a6dd3b48fab9f2b4be6613fbaea97d11e60
donarazhar <donarazhar@gmail.com>
2026-06-14 19:25:29 +0700

    feat: implement service worker for offline caching and PWA support

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'presensigps-cache-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/assets/css/inc/bootstrap/bootstrap.min.css',
+  '/assets/js/lib/bootstrap.min.js',
+  '/assets/img/logo-ypi.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch (wajib ada agar PWA terdeteksi)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cache if found, else fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});

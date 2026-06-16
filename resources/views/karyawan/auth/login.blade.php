cffdc49ad8ced15ecaef7d49f54c52aa97857680
donarazhar <donarazhar@github.com>
2026-06-16 08:14:41 +0700

    feat: add responsive login page view for employees

--- resources/views/karyawan/auth/login.blade.php
+++ resources/views/karyawan/auth/login.blade.php
@@ -192,7 +192,7 @@
             color: #065F46;
         }
 
-        .alert svg { flex-shrink: 0; margin-top: 1px; }
+        .alert svg { flex-shrink: 0; margin-top: 1px; width: 18px; height: 18px; min-width: 18px; }
 
         /* ── GOOGLE BUTTON ── */
         .btn-google {
@@ -404,21 +404,21 @@
         {{-- Alerts --}}
         @if(session('error'))
         <div class="alert alert-error">
-            <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/></svg>
+            <svg width="18" height="18" viewBox="0 0 20 20" fill="currentColor" style="flex-shrink:0;"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/></svg>
             <span>{{ session('error') }}</span>
         </div>
         @endif
 
         @if(session('success'))
         <div class="alert alert-success">
-            <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/></svg>
+            <svg width="18" height="18" viewBox="0 0 20 20" fill="currentColor" style="flex-shrink:0;"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/></svg>
             <span>{{ session('success') }}</span>
         </div>
         @endif
 
         @if($errors->any())
         <div class="alert alert-error">
-            <svg viewBox="0 0 20 20" fill="currentColor" style="flex-shrink:0;margin-top:1px;"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/></svg>
+            <svg width="18" height="18" viewBox="0 0 20 20" fill="currentColor" style="flex-shrink:0;margin-top:1px;"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/></svg>
             <div>
                 @foreach($errors->all() as $error)
                     <div>{{ $error }}</div>
